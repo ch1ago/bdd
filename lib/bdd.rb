047ca7a817578890c1279752408e97af4830ad47
James Pinto <thejamespinto@gmail.com>
2016-05-30 15:38:17 -0300

    rails fix, bump to version 0.1.4

--- lib/bdd.rb
+++ lib/bdd.rb
@@ -8,10 +8,10 @@ end
 require "bdd/version"
 require "bdd/class_methods"
 
-if defined?(::RSpec) && !defined?(Bdd::RSpec)
+if defined?(::RSpec::Core) && !defined?(Bdd::RSpec)
   require 'bdd/rspec'
 end
 
-if defined?(::Minitest) && !defined?(Bdd::Minitest)
+if defined?(::Minitest::Reporter) && !defined?(Bdd::Minitest)
   require 'bdd/minitest'
 end
