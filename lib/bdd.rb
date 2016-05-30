b88a13f83ce492387bc6c02156d36a548e67ed6b
James Pinto <thejamespinto@gmail.com>
2016-05-30 15:16:10 -0300

    loading rspec or minitest instead of halting

--- lib/bdd.rb
+++ lib/bdd.rb
@@ -3,10 +3,15 @@ module Bdd
   autoload :Colors, 'bdd/colors'
   autoload :StringBuilder, 'bdd/string_builder'
   autoload :Title, 'bdd/title'
-  autoload :Check, 'bdd/check'
 end
 
 require "bdd/version"
 require "bdd/class_methods"
 
-Bdd::Check.new
+if defined?(::RSpec) && not defined?(Bdd::RSpec)
+  require 'bdd/rspec'
+end
+
+if defined?(::Minitest) && not defined?(Bdd::Minitest)
+  require 'bdd/minitest'
+end
